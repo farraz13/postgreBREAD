5f5e2444cdd8df6c22c60f48aa8c71aa57352d8a
farraz13 <farraz.f13@gmail.com>
2022-10-25 21:45:18 +0700

    ongoing

--- EXPRESS/routes/index.js
+++ EXPRESS/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
 
 /* GET home page. */
 
@@ -8,7 +9,7 @@ module.exports = function (db) {
   router.get('/', function (req, res, next) {
     db.query('SELECT * FROM tasks', (err, data) => {
       if (err) return res.send(err, 'error bang')
-      res.render('list', { tasks: data.rows })
+      res.render('list', { tasks: data.rows, moment })
     })
   });
 
