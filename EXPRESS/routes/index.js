da33d376c604206bb0164ea4fe7cfd390bd60bde
farraz13 <farraz.f13@gmail.com>
2022-10-26 00:45:49 +0700

    ongoing

--- EXPRESS/routes/index.js
+++ EXPRESS/routes/index.js
@@ -14,8 +14,18 @@ module.exports = function (db) {
   });
 
   router.get('/add', function (req, res, next) {
-      res.render('form', {})
+    res.render('form')
+  })
+
+
+  router.post('/add', function (req, res, next) {
+    const { string, integer, float, date, boolean } = req.body
+    console.log(req.body)
+    db.query('INSERT INTO tasks(string, integer, float, date, boolean) values ($1, $2, $3, $4, $5)', [string, integer, float, date, boolean], (err) => {
+      if (err) return res.send(err, 'error bang')
+      res.redirect('/')
     })
-  
+  })
+
   return router;
 }
