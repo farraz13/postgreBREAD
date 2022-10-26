367a01fc2fe0fa4d9b91da5b533847e36eb22b4e
farraz13 <farraz.f13@gmail.com>
2022-10-26 16:56:53 +0700

    ongoing

--- EXPRESS/routes/index.js
+++ EXPRESS/routes/index.js
@@ -14,7 +14,7 @@ module.exports = function (db) {
   });
 
   router.get('/add', function (req, res, next) {
-    res.render('form')
+    res.render('form',{item: {}, moment})
   })
 
 
@@ -26,6 +26,31 @@ module.exports = function (db) {
       res.redirect('/')
     })
   })
+
+  router.get('/edit/:id', function (req, res, next) {
+    db.query('SELECT * FROM tasks WHERE id =$1', [Number(req.params.id)], (err, data) => {
+      if (err) return res.send(err, 'error bang')
+      if(data.rows.length == 0)return res.send(err, 'data not found')
+      res.render('form', {item: data.rows[0], moment })
+    })
+  });
+
+  router.post('/edit/:id', function (req, res, next) {
+    const { string, integer, float, date, boolean } = req.body
+    console.log(req.body)
+    db.query('UPDATE tasks SET string = $1, integer = $2, float = $3, date = $4, boolean = $5, id = $6  ', [string, integer, float, date, boolean, Number(req.params.id)], (err) => {
+      if (err) return res.send(err, 'error bang')
+      res.redirect('/')
+    })
+  })
+
+  router.get('/delete/:id', function (req, res, next) {
+    db.query('DELETE FROM tasks WHERE id =$1', [Number(req.params.id)], (err) => {
+      if (err) return res.send(err, 'error bang')
+      res.redirect('/')
+    });
+  });
+
 
   return router;
 }
